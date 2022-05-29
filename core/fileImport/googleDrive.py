fb2059bf3037bc80387f28ba70b0a4148e5c9c21
Jared Stemper <jaredastemper@gmail.com>
2022-05-28 23:09:06 -0500

    clarified exception process/language

--- core/fileImport/googleDrive.py
+++ core/fileImport/googleDrive.py
@@ -1,29 +1,12 @@
 from apiclient import discovery
-from google.oauth2 import service_account
-import google_auth_oauthlib
-
 from oauth2client.service_account import ServiceAccountCredentials
 from httplib2 import Http
 
-"""
-TODO:
-	follow: https://www.geeksforgeeks.org/get-list-of-files-and-folders-in-google-drive-storage-using-python/
-	pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
-"""
-# def auth():
-
-
 def main():
-	credentialsPath = './client_secrets.json'
+	credentialsPath = 'core/fileImport/client_secrets.json'
 
-	#View metadata for files in your Google Drive
-	#View the photos, videos and albums in your Google Photos
 	#See and download all your Google Drive files
 	scope = "https://www.googleapis.com/auth/drive.readonly"
-	# , \
-	# 	"https://www.googleapis.com/auth/drive",\
-	# 	"https://www.googleapis.com/auth/drive.metadata.readonly",\
-	# 	"https://www.googleapis.com/auth/drive.photos.readonly",\
 	
 	try:
 		credentials = ServiceAccountCredentials.from_json_keyfile_name(credentialsPath, scope)
@@ -44,7 +27,9 @@ def main():
 		for item in items:
 			print(u'{0} ({1})'.format(item['name'], item['id']))
 	except:
-		print("fail")
+		print("Failed to retrieve files. Refer to the following articles for troubleshooting:")
+		print("\thttps://developers.google.com/drive/api/v3/quickstart/python")
+		print("\thttps://www.geeksforgeeks.org/get-list-of-files-and-folders-in-google-drive-storage-using-python/")
 
 
 if __name__ == '__main__':
